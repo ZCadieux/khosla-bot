e2859d1701a6f799fb791a365d998bde5f79a23f
Poseyhead <gnomezach@gmail.com>
2020-12-04 13:37:12 -0800

    Added counter functionality

--- confessions.py
+++ confessions.py
@@ -1,10 +1,23 @@
-"""import os
-import random
-import discord
+import os
+# import random
+# import discord
 
-from discord.ext import commands
-from dotenv import load_dotenv"""
+# from discord.ext import commands
+# from dotenv import load_dotenv
 import config as c
+
+
+COUNT = os.path.expanduser("~\\Documents\\GitHub\\khosla-bot\\count.txt")
+
+
+def updateConfNum(newConfNum):
+    with open(COUNT, "w") as fd:
+        fd.write(str(newConfNum) + "\n")
+
+
+def retrieveConfNum():
+    with open(COUNT, "r") as fd:
+        return fd.readline().strip()
 
 # confessions
 
@@ -15,6 +28,9 @@ async def on_message(ctx):
     if ctx.author == c.bot.user:
         return
     if not ctx.guild:
-        await ctx.channel.send('Confession submitted!')
+        confNum = 1 + int(retrieveConfNum())
+        confText = f'Confession Number {confNum} submitted!'
+        await ctx.channel.send(confText)
         confessionApproval = c.bot.get_channel(784308011656675358)
+        updateConfNum(confNum)
         await confessionApproval.send(confession)
